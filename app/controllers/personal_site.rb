dcade535524d71a93960250792c3734cedd93d5d
Iza Jimenez <izavetha@gmail.com>
2017-09-25 15:04:41 -0600

    Iteration 3. Styling. All tests passing

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -4,16 +4,34 @@ class PersonalSite
   def self.call(env)
     case env["PATH_INFO"]
     when '/' then index
+    when '/about' then about
+    when '/main.css' then css
     else
       error
     end
   end
 
   def self.index
-    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+    render_view('index.html')
+  end
+
+  def self.about
+    render_view('about.html')
   end
 
   def self.error
-    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
+    render_view('error.html', '404')
   end
-end
+
+  def self.render_view(page, code = '200')
+    [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
+  end
+  
+  def self.css
+     render_static('main.css')
+   end
+
+   def self.render_static(asset)
+     [200, {'Content-Type' => 'text/html'}, [File.read("./public/#{asset}")]]
+   end
+ end
